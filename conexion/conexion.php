8b3ac41f5940312e0d5e4c90edf52e4385d9c306
Raul UBUNTU-Casa <alarmarsp@gmail.com>
2015-12-24 17:48:17 -0300

    Cambiado conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -4,12 +4,12 @@ $hostname_indices = "localhost";
 //$database_indices = "rspeirl_indices_parral";  //SERVIDOR RSP.CL
 //$username_indices = "rspeirl";
 //$password_indices = "sg91829182";
-//$database_indices = "cbrparral";   //SERVIDOR UBUNTU CASA
-//$username_indices = "root";
-//$password_indices = "1234";
-$database_indices = "cbrparral";   //SERVIDOR NOTEBOOK RAUL
+$database_indices = "cbrparral";   //SERVIDOR UBUNTU CASA
 $username_indices = "root";
-$password_indices = "";
+$password_indices = "1234";
+//$database_indices = "cbrparral";   //SERVIDOR NOTEBOOK RAUL
+//$username_indices = "root";
+//$password_indices = "";
 
 
 $conexion = mysqli_connect($hostname_indices, $username_indices, $password_indices, $database_indices) or die("Error ***** no se conecto al servidor... " . mysqli_error($conexion)); 
